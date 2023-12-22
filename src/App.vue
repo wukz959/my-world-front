117dee842ab0720152c192ed77061333e79ab89d
wukz <275115747@qq.com>
2023-12-22 19:32:23 +0800

    fix mobile

--- src/App.vue
+++ src/App.vue
@@ -5,11 +5,19 @@
 </template>
 
 <script>
+import isMobile from '@/mixins/isMobile'
 export default {
   name: 'App',
+  mixins: [isMobile],
   mounted () {
+    if (this.isMobile()) {
+      document.documentElement.style.setProperty('--bg-color-black', 'rgba(0, 0, 0, 0)')
+    } else {
+      document.documentElement.style.setProperty('--bg-color-black', '#2c2635')
+    }
   }
 }
 </script>
 <style lang="less">
+
 </style>
--- src/mixins/isMobile.js
+++ src/mixins/isMobile.js
@@ -1,8 +1,24 @@
 export default {
   created () {},
   methods: {
+    // isMobile () {
+    //   return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)
+    // }
     isMobile () {
-      return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)
+      // 检查用户代理字符串
+      if (/Android|webOS|iPhone|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+        return true
+      }
+      // 检查iPad的用户代理字符串
+      if (/iPad/i.test(navigator.userAgent) && ('ontouchstart' in window || navigator.maxTouchPoints > 0)) {
+        return true
+      }
+      // 检查屏幕宽度和触摸功能
+      if (window.innerWidth <= 1024 && ('ontouchstart' in window || navigator.maxTouchPoints > 0)) {
+        return true
+      }
+      return false
     }
+
   }
 }
